a4c401c9aaab73568ac3de088a18140a752f4d2c
Muhammad Hidayat Mauluddin <71598808+mauluddin12z@users.noreply.github.com>
2026-05-12 19:34:34 +0700

    Rename ecosystem.config.cjs to ecosystem.config.js

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -1,4 +1,4 @@
-module.exports = {
+export default {
     apps: [
         {
             name: "sales-page-builder-frontend",
@@ -10,4 +10,4 @@ module.exports = {
             }
         }
     ]
-}
+};
